929d5c7e0107a8ca26a244d5d990c7b5f00778da
unknown <ijlnflhbrzdev@gmail.com>
2022-06-03 14:40:14 +0700

    +[UPDATE] Change tag href to button

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ var flkty = new Flickity(elem, {
 var flkty = new Flickity('.brgs-carousel', {
      // options
 });
+
+$("#targetBtn1").click(function () {
+     $('html,body').animate({
+          scrollTop: $("#cta-mulai-berjualan").offset().top
+     },
+          'slow');
+});
+
+$("#targetBtn2").click(function () {
+     $('html,body').animate({
+          scrollTop: $("#simulasi-penghasilan").offset().top
+     },
+          'slow');
+});
